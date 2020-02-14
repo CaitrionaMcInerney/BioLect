230b2f69266d97af9f50b4c7d29cf83a537436af
CaitrionaMcInerney <CaitrionaMcInerney@users.noreply.github.com>
2020-02-14 11:16:32 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="microBioLect",
     version="0.0.1",
-    author="Jessica Blair",
-    author_email="jblair22@qub.ac.uk",
+    author="Darragh McArt",
+    author_email="d.mcart@qub.ac.uk",
     description="A natural language syntax domain specific language tool for simple and automated analysis "
                 "of microarray data",
-    url="https://github.com/JessBlairQUB/BioLect",
+    url="https://github.com/CaitrionaMcInerney/BioLect",
     packages=setuptools.find_packages(),
     classifiers=["Programming Language :: Python :: 3",
                  "License :: ",
